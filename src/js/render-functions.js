13e7550b002c5b670304d87029c94a8d76a5af97
Oleksandr Haponov <olex.haponov@gmail.com>
2025-06-04 19:00:23 +0300

    Summary

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,21 +2,23 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
-
-const lightbox = new SimpleLightbox('.gallery a');
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 export function createGallery(images) {
   const markup = images
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-      <li>
-        <a href="${largeImageURL}">
-          <img src="${webformatURL}" alt="${tags}" />
+      <li class="gallery__item">
+        <a class="gallery__link" href="${largeImageURL}">
+          <img class="gallery__image" src="${webformatURL}" alt="${tags}" />
         </a>
-        <div>
-          <p>Likes: ${likes}</p>
-          <p>Views: ${views}</p>
-          <p>Comments: ${comments}</p>
-          <p>Downloads: ${downloads}</p>
+        <div class="info">
+          <p><b>Likes:</b> ${likes}</p>
+          <p><b>Views:</b> ${views}</p>
+          <p><b>Comments:</b> ${comments}</p>
+          <p><b>Downloads:</b> ${downloads}</p>
         </div>
       </li>
     `)
